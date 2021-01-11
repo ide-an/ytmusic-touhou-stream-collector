b8944c0f16da83c2c7ae197fa57c3ba20eef638b
ide-an <ideanweb@gmail.com>
2021-01-11 16:15:35 +0900

    正規化など追加

--- collector.py
+++ collector.py
@@ -4,6 +4,9 @@ import typing
 from time import sleep
 from datetime import datetime
 import unicodedata
+import re
+
+is_debug = False
 
 class Seed(typing.NamedTuple):
     collection_name:str
@@ -27,10 +30,14 @@ def create_ytmusic_url(album, track):
 albums_cache = {}
 def get_albums(yt, collection_name, artist_name):
     # マイナス検索にならないようにハイフンを置換
-    search_key = (collection_name + " " + artist_name).replace("-", " ")
+    search_key = (collection_name).replace("-", " ")
+    # EPの削除（狐夢想屋対応）
+    search_key = re.sub("EP$", "", search_key)
+    if is_debug:
+        print("{}: search_key:{}".format(datetime.now(), search_key))
     if search_key not in albums_cache:
         # TODO: 検索結果が複数ページの場合の対処
-        albums_cache[search_key] = yt.search(search_key, 'albums')
+        albums_cache[search_key] = yt.search(search_key, 'albums', limit=100)
     return albums_cache[search_key]
 
 album_detail_cache = {}
@@ -42,29 +49,42 @@ def get_album_detail(yt, browseId):
 def normalize(s):
     # unicode正規化
     s = unicodedata.normalize('NFKC', s)
+    # 大文字小文字
+    s = s.lower()
     # ~と～の表記ゆれ
     s = s.replace('~',' ').replace('\u301C',' ').replace('\uFF5E',' ')
     # ()のあるなし
     s = s.replace('(','').replace(')','').replace('（','').replace('）','')
     s = s.replace('[','').replace(']','')
+    # 空白のあるなし
+    s = s.replace(' ','').replace('　','')
+    # EPの削除（狐夢想屋対応）
+    s = re.sub("-ep$", "", s)
     return s
 
 def search_ytmusic(yt:ytmusicapi.YTMusic, seed:Seed):
     # 該当するアルバムを見つける→該当する曲を見つける
     albums = get_albums(yt, seed.collection_name, seed.artist_name)
-    if len(albums) == 0:
-        return None
+    album_found = False
     for album in albums:
-        #pprint(album)
+        if is_debug:
+            pprint(album)
         # apple musicでのアーティスト名は必ずしもyoutube musicと一致しない。そのためアーティスト名の判定はしない
         # 例： 「蓬莱人形 ~ Dolls in Pseudo Paradise」のアルバムアーティストは「ZUN」ではなく「上海アリス幻樂団」
-        if album["title"] == seed.collection_name:
+        if is_debug:
+            pprint(normalize(album["title"]))
+            pprint(normalize(seed.collection_name))
+        if normalize(album["title"]) == normalize(seed.collection_name):
+            album_found = True
             album_detail = get_album_detail(yt, album["browseId"])
-            #pprint(album_detail)
+            if is_debug:
+                pprint(album_detail)
             for track in album_detail["tracks"]:
                 #pprint(track)
                 if normalize(track["title"]) == normalize(seed.track_name):
                     return create_ytmusic_url(album, track)
+    if not album_found:
+        print("{}: album not found:{}".format(datetime.now(), seed))
     return None
 
 def output_line(f_out, seed:Seed, ytmusic_result:YTMusicResult):
@@ -100,14 +120,15 @@ def main():
                 is_header = False
                 continue
             seed = Seed._make(line.rstrip().split("\t"))
-            #pprint(seed)
+            if is_debug:
+                pprint(seed)
             result = search_ytmusic(yt, seed)
             output_line(f_out, seed, result)
             i += 1
             if i%SLEEP_COUNT == 0:
                 print("{}: done {} songs...".format(datetime.now(), i))
                 sleep(SLEEP_SECONDS)
-            if i > 100:
+            if i > 1000:
                 break
     print("{}: end collector".format(datetime.now()))
 
